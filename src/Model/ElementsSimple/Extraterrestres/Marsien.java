7cbf073f48d30692137bcdb46f5860b81bde9899
FabioPomarelli <fabio.pomarelli@gmail.com>
2019-03-28 14:33:55 +0100

    projet

---
+++ src/Model/ElementsSimple/Extraterrestres/Marsien.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Model.ElementsSimple.Extraterrestres;
+
+import Model.ElementsSimple.Extraterrestre;
+
+/**
+ *
+ * @author pom
+ */
+public class Marsien extends Extraterrestre{
+    
+}
---
+++ src/Model/ElementsSimple/Personnes/Enterprise.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Model.ElementsSimple.Personnes;
+
+import Model.ElementSimple;
+
+/**
+ *
+ * @author pom
+ */
+public class Enterprise extends ElementSimple {
+    
+}
